732ff298db05ee681843643589afe936b9a32c4e
Saad-Surve <95091209+Saad-Surve@users.noreply.github.com>
2023-12-02 20:06:06 +0530

    Authentication done

--- client/src/components/UI/card.jsx
+++ client/src/components/UI/card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Card = () => {
   return (
-    <div className="card lg:card-side bg-white flex shadow-xl">
+    <div className="card lg:card-side bg-blue-100 flex shadow-xl">
         <figure className='border-inherit rounded-10 border-2'><img src="/images/nyaaydoot.png" alt="Album" /></figure>
             <div className="card-body">
                 <h2 className="card-title">New album is released!</h2>
--- server/utils/utils.js
+++ server/utils/utils.js
@@ -1,7 +1,32 @@
 const jwt = require('jsonwebtoken');
+const multer = require('multer')
+const path = require('path')
 const generateToken = (id) =>{
     return jwt.sign({ id },process.env.JWT_SECRET,{
         expiresIn:'1d'
     })
 }
-module.exports = generateToken
+const fileStorage = multer.diskStorage({
+    destination:'uploads',
+    filename:(req,file,cb)=>{
+        cb(null,file.fieldname+'_'+req.body.username+path.extname(file.originalname))
+    }
+})
+
+const uploadImage = multer({
+    storage:fileStorage,
+    limits:{
+        fileSize:1000000
+    },
+    fileFilter(req,file,cb){
+        if(!file.originalname.match(/\.(png|jpg)$/)){
+            return cb(new Error('Please upload an image file'));
+        }
+        cb(undefined,true)
+    }
+})
+module.exports = {
+    generateToken,
+    fileStorage,
+    uploadImage
+}
