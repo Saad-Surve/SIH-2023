212f971c00d250a1fb2b20bd608013836f5e48da
2102ankit <105378102+2102ankit@users.noreply.github.com>
2023-12-03 00:17:35 +0530

    Slight Change to Navbar

--- client/src/components/documents/Documents.jsx
+++ client/src/components/documents/Documents.jsx
@@ -5,66 +5,85 @@ const Documents = () => {
   const data = [
     {
       title: "PAN CARD",
-      description: "Pan card is a unique identification number assigned by the government to every individual resident of India.",
+      description:
+        "Pan card is a unique identification number assigned by the government to every individual resident of India.",
       link: "https://uidai.gov.in/",
-      image: "/images/nyaaydoot.png"
+      image: "/images/nyaaydoot.png",
     },
     {
       title: "PAN CARD",
-      description: "Pan card is a unique identification number assigned by the government to every individual resident of India.",
+      description:
+        "Pan card is a unique identification number assigned by the government to every individual resident of India.",
       link: "https://uidai.gov.in/",
-      image: "/images/nyaaydoot.png"
+      image: "/images/nyaaydoot.png",
     },
     {
       title: "PAN CARD",
-      description: "Pan card is a unique identification number assigned by the government to every individual resident of India.",
+      description:
+        "Pan card is a unique identification number assigned by the government to every individual resident of India.",
       link: "https://uidai.gov.in/",
-      image: "/images/nyaaydoot.png"
-    },{
-      title: "PAN CARD",
-      description: "Pan card is a unique identification number assigned by the government to every individual resident of India.",
-      link: "https://uidai.gov.in/",
-      image: "/images/nyaaydoot.png"
-    },{
-      title: "PAN CARD",
-      description: "Pan card is a unique identification number assigned by the government to every individual resident of India.",
-      link: "https://uidai.gov.in/",
-      image: "/images/nyaaydoot.png"
-    },{
-      title: "PAN CARD",
-      description: "Pan card is a unique identification number assigned by the government to every individual resident of India.",
-      link: "https://uidai.gov.in/",
-      image: "/images/nyaaydoot.png"
-    },{
-      title: "PAN CARD",
-      description: "Pan card is a unique identification number assigned by the government to every individual resident of India.",
-      link: "https://uidai.gov.in/",
-      image: "/images/nyaaydoot.png"
-    },{
-      title: "PAN CARD",
-      description: "Pan card is a unique identification number assigned by the government to every individual resident of India.",
-      link: "https://uidai.gov.in/",
-      image: "/images/nyaaydoot.png"
-    },{
-      title: "PAN CARD",
-      description: "Pan card is a unique identification number assigned by the government to every individual resident of India.",
-      link: "https://uidai.gov.in/",
-      image: "/images/nyaaydoot.png"
+      image: "/images/nyaaydoot.png",
     },
-  ]
-  return <div className="flex flex-col">
-    <Navbar />                   
-    <div className=" p-10 h-min flex justify-center flex-col gap-6 flex-wrap grid grid-cols-3">
-    {data.map((item,index )=>(
-      <Card 
-      key={index}
-      title={item.title}
-      description={item.description}
-      link={item.link}
-      image={item.image}/>
-    ))}
-  </div>;
-  </div>
+    {
+      title: "PAN CARD",
+      description:
+        "Pan card is a unique identification number assigned by the government to every individual resident of India.",
+      link: "https://uidai.gov.in/",
+      image: "/images/nyaaydoot.png",
+    },
+    {
+      title: "PAN CARD",
+      description:
+        "Pan card is a unique identification number assigned by the government to every individual resident of India.",
+      link: "https://uidai.gov.in/",
+      image: "/images/nyaaydoot.png",
+    },
+    {
+      title: "PAN CARD",
+      description:
+        "Pan card is a unique identification number assigned by the government to every individual resident of India.",
+      link: "https://uidai.gov.in/",
+      image: "/images/nyaaydoot.png",
+    },
+    {
+      title: "PAN CARD",
+      description:
+        "Pan card is a unique identification number assigned by the government to every individual resident of India.",
+      link: "https://uidai.gov.in/",
+      image: "/images/nyaaydoot.png",
+    },
+    {
+      title: "PAN CARD",
+      description:
+        "Pan card is a unique identification number assigned by the government to every individual resident of India.",
+      link: "https://uidai.gov.in/",
+      image: "/images/nyaaydoot.png",
+    },
+    {
+      title: "PAN CARD",
+      description:
+        "Pan card is a unique identification number assigned by the government to every individual resident of India.",
+      link: "https://uidai.gov.in/",
+      image: "/images/nyaaydoot.png",
+    },
+  ];
+  return (
+    <div className="flex flex-col w-full">
+      <Navbar />
+      <div className=" p-10 h-min justify-center flex-col gap-6 flex-wrap grid grid-cols-3">
+        {data.map((item, index) => (
+          <Card
+            key={index}
+            title={item.title}
+            description={item.description}
+            link={item.link}
+            image={item.image}
+          />
+        ))}
+      </div>
+      ;
+    </div>
+  );
 };
 
 export default Documents;
