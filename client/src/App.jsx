44f66dddf546427191f39e6c04a52a303c07dbee
2102ankit <105378102+2102ankit@users.noreply.github.com>
2023-12-02 15:39:22 +0530

    Sidebar Styles and Routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,44 @@
-import './App.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import RootLayout from './pages/RootLayout'
-import { Home } from './components/home/Home'
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./pages/RootLayout";
+import Home from "./components/home/Home";
+import Documents from "./components/documents/Documents";
+import News from "./components/news/News";
+import Directory from "./components/directory/Directory";
+import Community from "./components/community/Community";
 
 const router = createBrowserRouter([
   {
-    path:'/',
-    element:<RootLayout />,
-    children:[
-      { index: true, element:<Home /> },
-    ]
-  }
-])
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <Home /> },
+      {
+        path: "documents",
+        element: <Documents />,
+      },
+      {
+        path: "news",
+        element: <News />,
+      },
+      {
+        path: "directory",
+        element: <Directory />,
+      },
+      {
+        path: "community",
+        element: <Community />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
-    <>
-      <RouterProvider router={router}/>      
-    </>
-  )
+    <div className="bg-background-gray">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
-export default App
+export default App;
