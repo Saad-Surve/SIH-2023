8f286e7e8a64aac1faa015b88b791ecac45e30e9
Yusufsodawala786 <youzarsifsodawala7@gmail.com>
2023-12-04 00:30:56 +0530

    directory page done

--- client/src/components/directory/Directory.jsx
+++ client/src/components/directory/Directory.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import Navbar from "../UI/navbar";
 import { Icon } from "@iconify/react";
 import LawyerCard from "./lawyerCard";
+import Consult from "./consult";
+import {ScrollShadow} from "@nextui-org/react";
 
 const Directory = () => {
   return (
     <div className="w-full flex flex-col h-screen overflow-y-auto scrollbar-hide">
       <Navbar />
-      <div className="h-full flex flex-row">
-        <div className="max-w-7/12 flex flex-col p-10 top-0 sticky">
-          <div className="flex flex-row p-10 m-1 border rounded-lg bg-blue-100 items-center font-extrabold text-base z-10 ">
+      <div className="h-[89%] flex flex-row">
+        <div className="max-w-8/12 flex flex-col p-10 top-0 sticky ">
+          <div className="flex flex-row p-10 m-1 border border-b-3 drop-shadow-sm  rounded-lg bg-blue-200 items-center w-[100%font-extrabold text-base z-10 ">
             <div className="flex flex-row justify-center items-center border rounded-full p-2 border-white bg-white">
               <Icon icon="tabler:phone" color="rgb(0,106,255)" fontSize={25} />
             </div>
@@ -31,15 +33,17 @@ const Directory = () => {
             </div>
             <span className="text-center">Consult Multiple Lawyers</span>
           </div>
-          <div className="items-center p-5 gap-4 z-0 overflow-y-auto h-[70%] scrollbar-hide">
+          <div className="p-5 gap-4 z-0 overflow-y-auto h-[70%] scrollbar-hide">
+          <ScrollShadow >
             <LawyerCard />
             <LawyerCard />
             <LawyerCard />
             <LawyerCard />
+          </ScrollShadow>
           </div>
         </div>
-        <div className="w-5/12 flex bg-blue-300">
-
+        <div className="w-5/12 flex justify-center items-center">
+            <Consult />
         </div>
       </div>
     </div>
