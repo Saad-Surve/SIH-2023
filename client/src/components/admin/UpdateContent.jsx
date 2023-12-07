81554f03a27f3568603e26cdde9fde560aa72eaa
Yusufsodawala786 <youzarsifsodawala7@gmail.com>
2023-12-07 19:34:00 +0530

    Update content page done

--- client/src/components/admin/UpdateContent.jsx
+++ client/src/components/admin/UpdateContent.jsx
@@ -45,60 +45,61 @@ const UpdateContent = () => {
     },
   ];
   return (
-    <div className="w-full  flex justify-center items-center relative">
+    <div className="w-full  flex justify-center items-center relative h-screen">
       <div
-        className="bg-cover bg-center w-full h-screen absolute opacity-90 bg-gradient-to-t brightness-[25%] saturate-150"
+        className="bg-cover bg-center w-full h-full absolute opacity-90 bg-gradient-to-t brightness-[25%] saturate-150"
         style={{ backgroundImage: `url(${admin})` }}
       ></div>
-      <div className="bg-white/[0.81] w-3/5 h-3/5 flex flex-col py-6 items-center justify-center  rounded-[40px] z-10 ">
-        <div className="h-1/5 w-4/5 text-center font-mulish">
-          Pending Update Requests
+      <div className="w-3/5 h-[90%] flex flex-col py-6 items-center justify-center  rounded-[40px] z-10 ">
+        <div className="w-[90%] h-1/5 bg-white rounded-lg flex flex-col justify-center items-center">
+          <div className="w-full text-center font-saira pt-4">
+            Pending Update Requests
+          </div>
+          <div className="w-[90%] flex items-center justify-evenly text-xl font-bold">
+            <div className="text-black p-4">Original Content</div>
+            <div className="mr-24">Proposed Changes</div>
+          </div>
         </div>
-        <ScrollShadow className="w-[90%] text-[#585874]  max-h-3/5 admin font-bold overflow-y-auto text-xl grid grid-cols-5">
-          <div className="border-b-2  col-span-2 text-black relative border-[#6E6E91]">
-            <span className="text-xl text-center absolute left-1/2 top-1/2 w-full p-2 -translate-x-1/2 -translate-y-1/2">
-              Original Content
-            </span>
-          </div>
-          <div className="border-b-2 col-span-2 text-black relative border-[#6E6E91]">
-            <span className="text-xl text-center absolute left-1/2 top-1/2 w-full p-2 -translate-x-1/2 -translate-y-1/2">
-              Proposed Changes
-            </span>
-          </div>
-          <span className="border-b-2 border-[#6E6E91] p-4"></span>
+        <ScrollShadow className="w-[90%] text-text-gray h-max mt-4 pr-2 admin overflow-y-auto text-xl">
+          {/* <span className="border-b-2 border-[#6E6E91] p-4"></span> */}
           {data.map((item, index) => {
             return (
-              <>
-                <div className="border-b-2 relative col-span-2  border-[#6E6E91]">
-                  <Button
-                    onClick={() => {
-                      setTitle("Original Content");
-                      setContent(item.orginalContent);
-                      onOpen();
-                    }}
-                    className="text-xl text-center w-4/5 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 font-bold text-[#585874] bg-white rounded-xl p-1"
-                  >
-                    View Original Content
-                  </Button>
+              <div
+                key={index}
+                className="flex w-full justify-center items-center bg-light-blue rounded-md mb-2 pl-4"
+              >
+                <div className="w-full flex justify-evenly items-center text-lg  py-4 ">
+                  <div className="">
+                    <Button
+                      onClick={() => {
+                        setTitle("Original Content");
+                        setContent(item.orginalContent);
+                        onOpen();
+                      }}
+                      className="text-lg text-center text-header-black bg-white rounded-xl py-1.5 px-3 hover:bg-primary hover:text-white transition-all"
+                    >
+                      View Original Content
+                    </Button>
+                  </div>
+                  <div className="">
+                    <Button
+                      onClick={() => {
+                        setTitle("Proposed Changes");
+                        setContent(item.changes);
+                        onOpen();
+                      }}
+                      className="text-lg text-center text-header-black bg-white rounded-xl py-1.5 px-3  hover:bg-primary hover:text-white transition-all"
+                    >
+                      View Proposed Changes
+                    </Button>
+                  </div>
                 </div>
-                <div className="border-b-2 relative col-span-2 border-[#6E6E91]">
-                  <Button
-                    onClick={() => {
-                      setTitle("Proposed Changes");
-                      setContent(item.changes);
-                      onOpen();
-                    }}
-                    className="text-xl text-center w-4/5 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 font-bold text-[#585874]  bg-white rounded-xl p-1"
-                  >
-                    View Proposed Changes
-                  </Button>
-                </div>
-                <span className="border-b-2 border-[#6E6E91] p-4">
+                <div className="">
                   <Popover placement="left-start" showArrow={true}>
                     <PopoverTrigger>
                       <Icon
                         icon="pepicons-pencil:dots-y"
-                        className="ml-auto mr-6 h-6 w-6 hover:text-text-gray hover:border-1 hover:bg-gray-200 hover:rounded-[50%] hover:p-1 hover:h-8 hover:w-8 hover:mr-5 "
+                        className="ml-6 mr-6 h-6 w-6 hover:text-primary hover:border-1 hover:bg-white hover:rounded-[50%] hover:p-1 hover:h-8 hover:w-8 hover:mx-5 "
                       />
                     </PopoverTrigger>
                     <PopoverContent>
@@ -112,8 +113,8 @@ const UpdateContent = () => {
                       </Button>
                     </PopoverContent>
                   </Popover>
-                </span>
-              </>
+                </div>
+              </div>
             );
           })}
           <CustModal
