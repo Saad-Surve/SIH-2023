06c9b374baa983afcf0c04ce8b8f8b821e820406
nehagode23 <118003687+nehagode23@users.noreply.github.com>
2023-12-10 08:45:39 +0530

    did you know updated

--- client/src/components/home/didyouknow.js
+++ client/src/components/home/didyouknow.js
@@ -3,18 +3,18 @@ export const didyouknow = [
   "Right to Information Act: The Right to Information Act, 2005, allows citizens to access government information.",
   "Caste-Based Discrimination: The practice of untouchability is prohibited by the Indian Constitution.",
   "Indian Penal Code: The Indian Penal Code (IPC) was enacted in 1860 and remains a key criminal law in the country.",
-  "Judicial Activism: Indian courts, especially the Supreme Court, have a history of judicial activism, taking up social issues and environmental concerns.",
+  "Section 4 in the Maternity Benefit Act of 1961 states that a pregnant woman cannot be fired., and no employer/boss can fire a pregnant woman.",
   "Citizenship Amendment Act (CAA): The CAA, passed in 2019, grants citizenship to persecuted minorities from neighboring countries but has been a subject of controversy.",
   "Lal Batti: The use of red beacons on vehicles by politicians and government officials was banned in 2017.",
   "Adultery Law: In 2018, the Supreme Court decriminalized adultery, stating that it violates the right to equality.",
 
-  "Secular State: India is a secular state, which means the government does not favor any religion.",
+  "You can walk into any hotel for a glass of water or visiting the washroom.",
   "One Rank, One Pension (OROP): OROP is a demand by the armed forces veterans for uniform pension payments, irrespective of when they retired.",
 
   "National Anthem in Theaters: In 2016, the Supreme Court mandated that the national anthem be played in movie theaters before film screenings.",
 
   "Legal Drinking Age: The legal drinking age varies from state to state, with some states setting it at 18 and others at 25.",
-  "Criminalization of Homosexuality: Homosexuality was decriminalized in India in 2018 when Section 377 of the IPC was struck down.",
+  "If an officer refuses to file your FIR, then she/he can be sentenced to jail for a period of 6 months to 1 year.",
 
   "Right to Property: The right to property was a fundamental right until 1978 when it was removed and converted into a legal right.",
 
@@ -53,8 +53,18 @@ export const didyouknow = [
   "Women in Armed Forces: Women were allowed permanent commission in all 10 branches of the Indian Army in 2020.",
   "Uniform Code of Pharmaceutical Marketing Practices (UCPMP): The UCPMP was introduced to regulate the marketing practices of pharmaceutical companies.",
   "Right to Legal Aid: Article 39A of the Indian Constitution mandates the state to provide free legal aid to the economically weaker sections.",
-  "The government, in 2013, took the initiative to ban all offensive and provocative images from social media sites",
-  "India is the only country in the world where both lions and tigers are found in the wild",
+  "According to the Motor Vehicle Act 1988, section -185 202, at the time of driving if your 100ml blood contains more than 30mg of alcohol, then the police can arrest you without a warrant.",
+  "A single male cannot adopt a female child.",
   "Women are not allowed to work at night in factories",
   "The Indian Treasury Fund act 1978 states that if you find a note worth Rs 10 or more while walking down the road and are unable to identify the owner, you must report the police official in that area.",
+  "The doctor's word on wether rape has happened is NOT the final call on evidence.",
+  "A rape survivor can approach a doctor for medical examination without filing an FIR",
+  "Under Section 294, singing lewd songs directed at women, in or near public place is considered sexual harassment.",
+  "In case of a cheque bounce, if you do not receive payment within 15 days of the legal notice, you can file criminal charges against the person and he might go to jail for it.",
+  "If one of the spouses voluntarily abandons his/her partner for at least a period of two years, the abandoned spouse can file a divorce case on the ground of desertion.",
+  "No person will be compelled to disclose his HIV status except with his informed consent, and if required by a court order.",
+  "According to Indian constitution, a person can have only and only Indian citizenship and if he/she takes citizenship of any other country then he/she is automatically not a citizen of India.",
+  "Section 46 of criminal procedure code : woman cannot be arrested before 6am and after 6 pm .",
+  "The Consumer Protection Act 1986 states that every consumer is entitles to recieve a complete refund of the consideration he paid, in the event that he has not been able to utilise the services that he had paid for.",
+  "As per the Indian treasure-trove act, if one discovers anything of value—exceeding Rs.10—hidden under the soil, then one must report it. If one fails to report, he/she is liable to punishment.",
 ];
