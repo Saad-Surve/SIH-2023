5af7e43687bd293e3fb43b9dcb009b31130fb008
Jayraj Menon <menonjayraj79@gmail.com>
2023-12-19 00:06:46 +0530

    merged the docs and resources tab

--- client/src/components/resources/Resources.jsx
+++ client/src/components/resources/Resources.jsx
@@ -9,6 +9,7 @@ import {
   Image,
 } from "@nextui-org/react";
 import "./resources.css";
+import { Navbar, NavbarContent, NavbarItem } from "@nextui-org/react";
 
 function CustCard({ logo, title, link, description }) {
   return (
@@ -38,6 +39,28 @@ function CustCard({ logo, title, link, description }) {
   );
 }
 
+function CustNav() {
+  return (
+    <Navbar
+      position="static"
+      style={{ width: "100%", backgroundColor: "lightblue", color: "#fff" }}
+    >
+      <NavbarContent style={{ display: "flex", justifyContent: "center" }}>
+        <NavbarItem>
+          <Link color="foreground" href="/documents">
+            Legal Documents
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href="/resources" aria-current="page">
+            Legal Resources
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+}
+
 const Resources = () => {
   const data = [
     {
@@ -52,19 +75,22 @@ const Resources = () => {
       logo: "lawmin.gov.in/sites/all/themes/landj/images/emblem-dark.png",
       title: "Right to Information",
       link: "https://rti.gov.in/",
-      description: `Right to Information Act 2005 mandates timely response to citizen requests for government information. `,
+      description: `Right to Information Act 2005 mandates timely response to citizen requests for government information.It is an initiative taken by Department of Personnel and Training, Ministry of Personnel, Public Grievances and Pensions.`,
     },
     {
       logo: "https://ncsc.nic.in/upload/logo/logo.png",
       title: "First Information Report",
       link: "https://ncsc.nic.in/pages/display/135-how-to-file-a-police-fir-complaint-online",
-      description: `It is prepared when the police receive information about the commission of a cognizable offence. In simple words, it is a complaint lodged with the police by the victim of a cognizable offence or by someone on his or her behalf, but anyone can make such a report either orally or in writing to the police. People can also lodge an FIR through online medium.`,
+      description: `It is prepared when the police receive information about the commission of a cognizable offence. Anyone can make such a report either orally or in writing to the police and also lodge an FIR through online medium.`,
     },
     {
       logo: "https://www.indiacode.nic.in/image/final-logo_update.jpg",
       title: "India Code",
       link: "https://www.indiacode.nic.in/",
-      description: `Digital Repository of all Laws: A system of laws for Communication`,
+      description: `Digital Repository of all Laws: A system of laws for Communication. You may Browse Central Acts, 
+      State Acts, 
+      Repealed Acts
+      Spent Acts, etc.`,
     },
     {
       logo: "https://nalsa.gov.in/lsams/images/logo-nlam.png",
@@ -76,7 +102,7 @@ const Resources = () => {
       logo: "https://d2f2ht52r1pvo3.cloudfront.net/static/dist/images/logo/dept-of-justice-logo-update.png",
       title: "Tele Law by Department of Justice",
       link: "https://www.tele-law.in/",
-      description: `Tele-Law means the use of communications and information technology for the delivery of legal information and advice. This e-interaction between lawyers and people would be through the video-conferencing infrastructure available at the CSCs.`,
+      description: `This e-interaction between lawyers and people would be through the video-conferencing infrastructure available at the CSCs.`,
     },
     {
       logo: "https://upload.wikimedia.org/wikipedia/commons/1/13/Logo_of_Income_Tax_Department_India.png",
@@ -95,24 +121,24 @@ const Resources = () => {
       title: "Citizen Rights Protection Council",
       link: "https://www.crpc.in/",
       description: `ABOUT CRPC:
-      Citizen Rights Protection Council (CRPC) is a civil rights’ group comprised of advocates, social activists and grassroots para- legal social workers dedicated to using the legal system to protect and advance the civil and human rights in India. CRPC, a non- profit and governmental civil rights’ group was set up in 2013 to defend the rights of the underprivileged sections of the society.`,
+      Citizen Rights Protection Council (CRPC) is a civil rights’ group comprised of advocates, social activists and grassroots para- legal social workers dedicated to using the legal system to protect and advance the civil and human rights in India.`,
     },
     {
       logo: "https://www.crpc.in/images/logo.png",
       title: "National Cyber Crime Reporting Portal",
       link: "https://cybercrime.gov.in/",
-      description: `This portal is an initiative of Government of India to facilitate victims/complainants to report cyber crime complaints online. This portal caters to complaints pertaining to cyber crimes only with special focus on cyber crimes against women and children. Complaints reported on this portal are dealt by law enforcement agencies/ police based on the information available in the complaints. It is imperative to provide correct and accurate details while filing complaint for prompt action.`,
+      description: `This portal is an initiative of Government of India to facilitate victims/complainants to report cyber crime complaints online. This portal caters to complaints pertaining to cyber crimes only with special focus on cyber crimes against women and children.`,
     },
   ];
 
   return (
-    <div className="resources">
-      <div className="heading">Important Links to Legal Resources</div>
-      <Divider />
-      <div className="cards">
+    <div className="w-full">
+      <CustNav />
+      <div className="text-center text-text-black font-bold bg-white top-0 sticky z-10 border-b-1 py-6 border-l shadow-md"></div>
+      <div className="p-10 py-6 justify-center flex-col gap-6 flex-wrap grid lg:grid-cols-3 z-5">
         {data.map((item) => {
           return (
-            <div key={item.title} className="eachCard">
+            <div key={item.title} className="border-4 border-black">
               <CustCard
                 logo={item.logo}
                 title={item.title}
